16166210ebf95cd1db54d3ce26ca3c481e5ba794
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2024-08-16 19:04:08 +0200

    Pathfinding

---
+++ Baldini_Marco_Progetto_Finale_AIV/PathFinding/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Baldini_Marco_Progetto_Finale_AIV
+{
+    class Node
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Cost { get; private set; }
+        public Vector4 Color;
+        public List<Node> Neighbours { get; }
+
+        public Node(int x, int y, int cost)
+        {
+            X = x;
+            Y = y;
+            Cost = cost;
+            Neighbours = new List<Node>();
+        }
+
+        public void AddNeighbour(Node node)
+        {
+            Neighbours.Add(node);
+        }
+
+        public void RemoveNeighbour(Node node)
+        {
+            Neighbours.Remove(node);
+        }
+
+        public void SetCost(int cost)
+        {
+            Cost = cost;
+        }
+    }
+}
